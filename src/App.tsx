382655ae321a2b5890c594c55bd6df30330e7be4
qdyyys <qdyyys@gmail.com>
2024-07-30 19:56:03 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 "./App.css";
-import { useState } from "react";
 import TextSaver from "./components/TextSaver";
 function App() {
-  const [userInput, setUserInput] = useState("");
-
-  const handleGo = () => {
-    console.log(userInput);
-  };
-
   return (
     <>
       <TextSaver />
